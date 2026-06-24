fe5b402074cedd0baebd34b0c0acd38c0d069251
RONIN-46 <prayagkn05@gmail.com>
2026-06-24 14:45:25 +0530

    Add PaddleOCR models and OCR module structure

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -69,6 +69,7 @@ dependencies {
     implementation("androidx.constraintlayout:constraintlayout:2.2.0")
     implementation("org.tensorflow:tensorflow-lite:2.14.0")
     implementation("org.tensorflow:tensorflow-lite-support:0.4.4")
+    implementation("com.baidu.paddle:paddle-lite-api:2.12")
 
 
 }
