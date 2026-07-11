29dd443830379c5a738d7f51a974719b00186e59
RONIN-46 <prayagkn05@gmail.com>
2026-07-11 17:53:02 +0530

    Implemented offline intent classifier

---
+++ app/src/main/java/com/krushna/divyadrishti/llm/IntentType.kt
@@ -0,0 +1,20 @@
+package com.krushna.divyadrishti.llm
+
+enum class IntentType {
+
+    SCENE,
+
+    OBJECT,
+
+    OCR,
+
+    FACE,
+
+    COLOR,
+
+    CURRENCY,
+
+    GENERAL,
+
+    UNKNOWN
+}
